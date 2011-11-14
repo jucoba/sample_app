0fe15108a2afefd21b4c15e6f3f0a2cfb0d5f0cd
Alberto Correa <alberto.correa@gmail.com>
2011-11-14 00:16:47 -0500

    Lot of thing a dont remember

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,12 @@ class PagesController < ApplicationController
   def about
     @title = "About"
   end
+
+  def help
+    @title = "Help"
+  end
+
+  def signin
+    @title = "Sign in"
+  end
 end
